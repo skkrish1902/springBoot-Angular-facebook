14aea616842af241031945964f899555a14d4d4f
Krishna, Madgula S <madgulasai95@gmail.com>
2022-11-30 14:05:35 +0530

    spring boot application initial commit

---
+++ springBoot-angular-facebook/src/main/java/springBootangularfacebook/SpringBootAngularFacebookApplication.java
@@ -0,0 +1,13 @@
+package springBootangularfacebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularFacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularFacebookApplication.class, args);
+	}
+
+}
